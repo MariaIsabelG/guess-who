6c265fc866760ae84af8a3af3203a43bd3b63da0
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-20 17:01:53 -0500

    time ran out

--- client.js
+++ client.js
@@ -5,14 +5,18 @@ $( document ).ready( onReady );
 function onReady(){
     console.log( 'jQuery is on!' );
     appendDivs();
+    randomNumber();
 };
 
 function appendDivs(){
     
-
     for( let i=0; i<people.length; i++ ){
         console.log( people[i] );
         $( 'body' ).append( `<img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}"></img>` )
     }
 
+};
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
 };
