013f610d79a70d26c51d288094d0e2b06c74faa5
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-20 16:46:49 -0500

    created appendDivs to add people to the DOM

--- client.js
+++ client.js
@@ -4,4 +4,14 @@ $( document ).ready( onReady );
 
 function onReady(){
     console.log( 'jQuery is on!' );
+    appendDivs();
+};
+
+function appendDivs(){
+    
+    for( let i=0; i<people.length; i++ ){
+        console.log( people[i] );
+        $( 'body' ).append( `<img src="https://github.com/${username}.png?size=250" alt="Profile image of ${name}"></img>` )
+    }
+
 }
